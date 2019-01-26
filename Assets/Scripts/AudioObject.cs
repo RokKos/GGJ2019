4b2dcbd656ebabdca35610d9a8cbd3a0eb63e406
Rok Kos <rocki5555@gmail.com>
2019-01-26 18:48:36 +0100

    Trigger Audio elements

---
+++ Assets/Scripts/AudioObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioObject : MonoBehaviour
+{
+    [SerializeField] AudioSource source;
+    
+
+    private void OnTriggerEnter(Collider other) {
+        source.Play();
+    }
+}
